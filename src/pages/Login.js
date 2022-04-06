aa853b6eb089d1f11935ac890cda5ee8e0f12df3
hariharan <hariharan.mct@gmail.com>
2022-04-06 18:31:27 +0530

    commit changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,7 +34,7 @@ function Login() {
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)} />
+                                onChange={(e) => setEmail(e.target.value)} required />
 
                         </Form.Group>
 
@@ -42,7 +42,7 @@ function Login() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)} />
+                                onChange={(e) => setPassword(e.target.value)} required />
                         </Form.Group>
                         <Button variant="primary" type="submit" disabled={isLoading}>
                             Login
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -36,21 +36,21 @@ function Signup() {
                             <Form.Label>Name</Form.Label>
                             <Form.Control type="text" placeholder="Enter your name"
                                 value={name}
-                                onChange={(e) => setName(e.target.value)} />
+                                onChange={(e) => setName(e.target.value)} required/>
                         </Form.Group>
                       
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)} />
+                                onChange={(e) => setEmail(e.target.value)} required/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)} />
+                                onChange={(e) => setPassword(e.target.value)} required />
                         </Form.Group>
 
                         <Button variant="primary" type="submit" disabled={isLoading}>
